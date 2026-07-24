650e8d3dc6ef42b33898b9af261350c90f8da43e
harshdeep1841 <harshdeepsingh10d@gmail.com>
2026-07-24 15:11:25 +0530

    assignement done

--- Learning/ProblemSolving/ArrayProblem.cs
+++ Learning/ProblemSolving/ArrayProblem.cs
@@ -223,6 +223,20 @@ public class ArrayProblem
         Console.WriteLine(sum);
         
     }
+
+    public static void print2DArray(int[,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    
     
     
 }
--- Main.cs
+++ Main.cs
@@ -61,8 +61,8 @@ public class MainCS
        
        //Problem Solving
 
-       int[] arr = { 1, 2, 2, 3, 3 };
-      
+     //  int[] arr = { 1, 2, 2, 3, 3 };
+     
        // for(int i = 0 ; i < arr.Length ; i++)
        // {
        //     arr[i] = int.Parse(Console.ReadLine());
@@ -82,50 +82,50 @@ public class MainCS
         // int int1 = Convert.ToInt32(Console.ReadLine());
         // Console.WriteLine(int1);
 
-      double double2 =   ArrayProblem.triangleArea(12, 12);
-      Console.WriteLine(double2);
-      
-      Console.WriteLine(ArrayProblem.anagram("HarSh" , "harsH"));
+     //  double double2 =   ArrayProblem.triangleArea(12, 12);
+     //  Console.WriteLine(double2);
+     //  
+     //  Console.WriteLine(ArrayProblem.anagram("HarSh" , "harsH"));
+     //
+     //  string[] strs = { "flower", "flow", "flight" };
+     //
+     //  string longestPrefix = ArrayProblem.LongestCommonPrefix(strs);
+     //  Console.WriteLine(longestPrefix);
+     //
+     // string rev =  ArrayProblem.reverse("EasyRewardz");
+     // Console.WriteLine(rev);
+     // int [] arr1 = { 1, 3, 2, 0, 1, 0, 2, 1, 0, 4 };
+     // ArrayProblem.moveZerosToEnd(arr1);
+     //
+     // foreach (int i in arr1)
+     // {
+     //     Console.Write(i + " ");
+     // }
 
-      string[] strs = { "flower", "flow", "flight" };
-
-      string longestPrefix = ArrayProblem.LongestCommonPrefix(strs);
-      Console.WriteLine(longestPrefix);
-
-     string rev =  ArrayProblem.reverse("EasyRewardz");
-     Console.WriteLine(rev);
-     int [] arr1 = { 1, 3, 2, 0, 1, 0, 2, 1, 0, 4 };
-     ArrayProblem.moveZerosToEnd(arr1);
-     
-     foreach (int i in arr1)
-     {
-         Console.Write(i + " ");
-     }
-
-     string s1 = new string("Harsh");
-     string s2 = "Harsh";
-     
-     if(s1 == s2) Console.WriteLine(s1); //both equals() and == check content not referance
-
-
-     int [ , ] array = new int[3 , 3]; //2d array
-
-     for (int i = 0; i < array.GetLength(0); i++) //GetLength gives us the length of particular dimension
-     {
-         for (int j = 0; j < array.GetLength(1); j++)
-         {
-            array[i, j] = i + j; 
-         }
-     }
-        Console.Error.WriteLine(array);
+     // string s1 = new string("Harsh");
+     // string s2 = "Harsh";
+     //
+     // if(s1 == s2) Console.WriteLine(s1); //both equals() and == check content not referance
+     //
+     //
+     // int [ , ] array = new int[3 , 3]; //2d array
+     //
+     // for (int i = 0; i < array.GetLength(0); i++) //GetLength gives us the length of particular dimension
+     // {
+     //     for (int j = 0; j < array.GetLength(1); j++)
+     //     {
+     //        array[i, j] = i + j; 
+     //     }
+     // }
+     //    Console.Error.WriteLine(array);
         
         
         
 
-       int[] array2 = { 1, 2, 3, 4 };
-       int [] copy = new int[array2.Length];
-       
-       Array.Copy(array2 , copy , 4);
+       // int[] array2 = { 1, 2, 3, 4 };
+       // int [] copy = new int[array2.Length];
+       //
+       // Array.Copy(array2 , copy , 4);
        //Array.Clear(array2 , 1 , 3);
       
        
@@ -146,10 +146,42 @@ public class MainCS
         //  int input = Convert.ToInt32(Console.ReadLine());
         // ArrayProblem.practiceProblem(input);
         
-        string inputString = Convert.ToString(Console.ReadLine());
-       char ch =  ArrayProblem.firstNonRepatingCharacter(inputString);
-       Console.WriteLine(ch);
+       //  string inputString = Convert.ToString(Console.ReadLine());
+       // char ch =  ArrayProblem.firstNonRepatingCharacter(inputString);
+       // Console.WriteLine(ch);
+       
+       //input 2d array
+       // string n = Console.ReadLine();
+       // string m = Console.ReadLine();
+       // int row;
+       // int column;
+       // if ((!int.TryParse(n, out row) || row < 0) || (!int.TryParse(m, out column) || column < 0))
+       // {
+       //     Console.Error.WriteLine("Invalid input");
+       //     return;
+       // }
+       //
+       // int [ , ] array2D = new int[row , column];
+       //
+       // for (int i = 0; i < array2D.GetLength(0); i++)
+       // {
+       //     for (int j = 0; j < array2D.GetLength(1); j++)
+       //     {
+       //         array2D[i, j] = Convert.ToInt32(Console.ReadLine());
+       //     }
+       // }
+       //
+       // ArrayProblem.print2DArray(array2D);
+    
+       Assignment1.WelcomeToBridgelabz();
+       Assignment1.AddTwoNumbers();
+       Assignment1.CelsiusToFahrenheit();
+       Assignment1.AreaOfCircle();
+       Assignment1.VolumeOfCylinder();
+       Assignment1.CalculateSimpleInterest();
+
 
     }
+
 }
 
