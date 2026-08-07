6acfebcc06118ae74cb6fe8e4ab32f4c0be27990
harshdeep1841 <harshdeepsingh10d@gmail.com>
2026-08-07 20:30:00 +0530

    worked on Nunit tesing

--- obj/Debug/net8.0/ConsoleAppLearning1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleAppLearning1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppLearning1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+906ef68866fbabab40c8508b8ad55ff482353610")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa243b84abe7baa0cd629ace218a8b482e3ca22b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppLearning1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppLearning1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
