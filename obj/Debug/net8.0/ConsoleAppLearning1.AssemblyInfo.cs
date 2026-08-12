4a816d73f74563cd33dcdc4152e7e2b4e4b9a4c6
harshdeep1841 <harshdeepsingh10d@gmail.com>
2026-08-12 23:36:21 +0530

    practice

--- obj/Debug/net8.0/ConsoleAppLearning1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleAppLearning1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppLearning1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45bdb0e0570d1364c8c31dd852cd022ac13209a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84502b0f6e958d48cb11710f38333bfbbd3102ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppLearning1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppLearning1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
