12321140755abdec67b506c2b10665b186884ac6
harshdeep1841 <harshdeepsingh10d@gmail.com>
2026-09-13 10:49:14 +0530

    worked on cursors and triggers

--- TestProject2/obj/Debug/net8.0/TestProject2.AssemblyInfo.cs
+++ TestProject2/obj/Debug/net8.0/TestProject2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6005fbe02e769836a9199f39e3c8bff018092f46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ccbbd2517baf9fe41ecdb8de7c36b5c6f90e947")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
